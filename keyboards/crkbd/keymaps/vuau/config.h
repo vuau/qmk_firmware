d91802415843f3e75d0c63ce06a375fddb83e521
Au Pham <apham@webmd.net>
2026-03-13 20:37:31 +0700

    my changes on macbook

---
+++ keyboards/crkbd/keymaps/vuau/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define TAPPING_TERM 200
+/* #define TAPPING_TOGGLE 1 */
+// #define PERMISSIVE_HOLD
+/* #define HOLD_ON_OTHER_KEY_PRESS */
+#define RETRO_TAPPING
+// #define QUICK_TAP_TERM 0  // Disable quick tap term
+// Disable inertia and constant speed configs, revert to QMK Default Accelerated Mode
+// Reset mouse key acceleration when tapping/overlapping keys to prevent erratic speed bursts
+#define MOUSEKEY_OVERLAP_RESET
+
+// (Optional) Tweak these defaults if you want it faster/slower than Atreus
+// #define MOUSEKEY_MAX_SPEED 10       // Default is 10
+// #define MOUSEKEY_TIME_TO_MAX 30     // Default is 30
---
+++ keyboards/keyboardio/atreus/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define TAPPING_TERM 150
+/* #define TAPPING_TOGGLE 1 */
+#define PERMISSIVE_HOLD
+/* #define HOLD_ON_OTHER_KEY_PRESS */
+#define RETRO_TAPPING
